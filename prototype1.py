d184afb543fea1ae3aa01d206fe88672792e24ef
AlasdairMcLean <alasdair.mclean@macaulay.cuny.edu>
2018-04-30 00:40:02 -0400

    refactored variable names, added some commenting

--- prototype1.py
+++ prototype1.py
@@ -5,9 +5,15 @@ from Raspi_PWM_Servo_Driver import PWM
 import time
 import RPi.GPIO as GPIO
 
-pwm = PWM(0x70)# Initialise the PWM device using the default address bmp = PWM(0x40, debug=True)
+motors = PWM(0x70)# Initialise the PWM device using the default address bmp = PWM(0x40, debug=True)
 servoMin = 300  # Min pulse length out of 4096
 servoMax = 450 # Max pulse length out of 4096
+
+MOTORMAX = 4096
+
+THMBCHANNEL=15
+FORECHANNEL=0
+MIDCHANNEL=1
 
 #def setServoPulse(channel, pulse):
   #pulseLength = 1000000                   # 1,000,000 us per second
@@ -21,13 +27,13 @@ servoMax = 450 # Max pulse length out of 4096
   #pwm.setPWM(channel, 0, pulse)
   
 #Thumb
-def finger1(n):
-    x=0
-    while (x<n):
-        pwm.setPWM(15, 0, 410)
+def finger1forward(totaltime):
+    timeelapsed=0 # initialize a counter to keep track of how many seconds have elapsed
+    while (timeelapsed<totaltime): #until the total time has elapsed,
+        motors.setPWM(THMBCHANNEL, 0, 410) # set the channel
         time.sleep(1)
-        x=x+1 
-    pwm.setPWM(15, 0 , 4096)
+        timeelapsed+=1
+    motors.setPWM(THMBCHANNEL, 0 , MOTORMAX)
 
     #GENERAL LOGIC:
     #while button 1 true then 
@@ -35,82 +41,82 @@ def finger1(n):
     #    time.sleep(1)
  
 
-def finger1_r(n):#thumb in reverse
-    x=0
-    while (x<n):#duration
-        pwm.setPWM(15, 0, servoMin)#going foward
+def finger1reverse(totaltime):#thumb in reverse
+    timeelapsed=0 # initialize a counter to keep track of how many seconds have elapsed
+    while (timeelapsed<totaltime): #until the total time has elapsed,
+        motors.setPWM(THMBCHANNEL, 0, servoMin) # set the channel
         time.sleep(1)
-        x=x+1#counter
-    pwm.setPWM(15, 0 , 4096)#shut off  
+        timeelapsed+=1#counter
+    motors.setPWM(THMBCHANNEL, 0 , MOTORMAX)#shut off  
 
 #if button 2 true then
 
 #forefinger
-def finger2(n):#forefinger
-    x=0
-    while (x<n):#duration
-        pwm.setPWM(0, 0, 410)#going foward
+def finger2forward(totaltime):#forefinger
+    timeelapsed=0 # initialize a counter to keep track of how many seconds have elapsed
+    while (timeelapsed<totaltime): #until the total time has elapsed,
+        motors.setPWM(FORECHANNEL, 0, 410) # set the channel
         time.sleep(1)
         
         #if button_press==False:
          #   print('Stopping now')
           #  pwm.setPWM(0, 0 , 4096)
            # time.sleep(1)
-        x=x+1#counter
-    pwm.setPWM(0, 0 , 4096)#shut off
-def finger2_r(n):#forefinger in reverse
-    x=0
-    while (x<n):#duration
-        pwm.setPWM(0, 0, servoMin)#going reverse
+        timeelapsed+=1#counter
+    motors.setPWM(FORECHANNEL, 0 , MOTORMAX)#shut off
+def finger2reverse(totaltime):#forefinger in reverse
+    timeelapsed=0 # initialize a counter to keep track of how many seconds have elapsed
+    while (timeelapsed<totaltime): #until the total time has elapsed,
+        motors.setPWM(FORECHANNEL, 0, servoMin) # set the channel
         time.sleep(1)
-        x=x+1#counter
-    pwm.setPWM(0, 0 , 4096)#shut off    
+        timeelapsed+=1#counter
+    motors.setPWM(FORECHANNEL, 0 , MOTORMAX)#shut off    
     
 #middle finger
-def finger3(n):
-    x=0
-    while (x<n):
-        pwm.setPWM(1, 0, 450)
+def finger3forward(totaltime):
+    timeelapsed=0 # initialize a counter to keep track of how many seconds have elapsed
+    while (timeelapsed<totaltime): #until the total time has elapsed,
+        motors.setPWM(MIDCHANNEL, 0, 450) # set the channel
         time.sleep(1)
-        x=x+1
-    pwm.setPWM(1, 0 , 4096)
+        timeelapsed+=1
+    motors.setPWM(MIDCHANNEL, 0 , MOTORMAX)
 
-def finger3_r(n):#middle finger in reverse 
-    x=0
-    while (x<n):#duration
-        pwm.setPWM(1, 0, servoMin)#going foward
+def finger3reverse(totaltime):#middle finger in reverse 
+    timeelapsed=0 # initialize a counter to keep track of how many seconds have elapsed
+    while (timeelapsed<totaltime): #until the total time has elapsed,
+        motors.setPWM(MIDCHANNEL, 0, servoMin)# set the channel
         time.sleep(1)
-        x=x+1#counter
-    pwm.setPWM(1, 0 , 4096)#shut off  
+        timeelapsed+=1#counter
+    motors.setPWM(MIDCHANNEL, 0 , MOTORMAX)#shut off  
     
 # first three fingers
-def all(n):
-    x=0
-    while (x<v):
-        pwm.setPWM(0, 0, servoMax)
-        pwm.setPWM(1, 0, servoMax)
-        pwm.setPWM(15, 0, servoMax)
-        x=x+1
+def allforward(totaltime):
+    timeelapsed=0 # initialize a counter to keep track of how many seconds have elapsed
+    while (timeelapsed<totaltime): #until the total time has elapsed,
+        motors.setPWM(THMBCHANNEL, 0, servoMax) # set the channel
+        motors.setPWM(FORECHANNEL, 0, servoMax)
+        motors.setPWM(MIDCHANNEL, 0, servoMax)
+        timeelapsed+=1
         time.sleep(1)
-    pwm.setPWM(0, 0 , 4096)
-    pwm.setPWM(1, 0 , 4096)
-    pwm.setPWM(15, 0 , 4096)
+    motors.setPWM(THMBCHANNEL, 0 , MOTORMAX)
+    motors.setPWM(FORECHANNEL, 0 , MOTORMAX)
+    motors.setPWM(MIDCHANNEL, 0 , MOTORMAX)
     
 #reverse all fingers
-def reverse(n):#all fingers
-    x=0
-    while (x<v):#duration
-        pwm.setPWM(0, 0, servoMin)
-        pwm.setPWM(1, 0, servoMin)
-        pwm.setPWM(15, 0, servoMin)
-        x=x+1
+def allreverse(totaltime):#all fingers
+    timeelapsed=0 # initialize a counter to keep track of how many seconds have elapsed
+    while (timeelapsed<totaltime): #until the total time has elapsed,
+        motors.setPWM(THMBCHANNEL, 0, servoMin) # set the channel
+        motors.setPWM(FORECHANNEL, 0, servoMin)
+        motors.setPWM(MIDCHANNEL, 0, servoMin)
+        timeelapsed+=1
         time.sleep(1)
-    pwm.setPWM(0, 0 , 4096)
-    pwm.setPWM(1, 0 , 4096)
-    pwm.setPWM(15, 0 , 4096)
+    motors.setPWM(THMBCHANNEL, 0 , MOTORMAX)
+    motors.setPWM(FORECHANNEL, 0 , MOTORMAX)
+    motors.setPWM(MIDCHANNEL, 0 , MOTORMAX)
 
 
-pwm.setPWMFreq(60)
+motors.setPWMFreq(60)
 # Set frequency to 60 Hz
 
 GPIO.setmode(GPIO.BOARD)
@@ -120,10 +126,9 @@ GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 # pwm.setPWM(0, 0, servoMin)
 # time.sleep(1)
 
-v= input('Enter the duration (s)')
-# y= input('Enter the strength')
-z=input('Select which finger you want to move: 1=thumb, 2=index, 3=middle, 4=all, 5=reverse motors')
-w=input('Select foward=0, reverse=1')
+timeinput= input('Enter the duration (s)')
+fingerinput=input('Select which finger you want to move: 1=thumb, 2=index, 3=middle, 4=all, 5=reverse motors')
+forwardOrReverse=input('Select foward=0, reverse=1')
 
 x=0
 on=4096
@@ -133,34 +138,34 @@ button_press = GPIO.input(7)
 
 if button_press==False:
         print('Stopping now')
-        pwm.setPWM(0, 0 , 4096)
+        motors.setPWM(FORECHANNEL, 0 , MOTORMAX)
         time.sleep(1)
 
-if int(z)==1:
-    if int(w)==0:
-        finger1(v)
-    if int(w)==1:
-        finger1_r(v)
+if int(fingerinput)==1:
+    if int(forwardOrReverse)==0:
+        finger1forward(timeinput)
+    if int(forwardOrReverse)==1:
+        finger1reverse(timeinput)
     #if button_press==False:
         #print('Stopping now')
         #pwm.setPWM(0, 0 , 4096)
         #time.sleep(1)
-if int(z)==2:
-    if int(w)==0:
-        finger2(v)
-    if int(w)==1:
-        finger2_r(v)
+if int(fingerinput)==2:
+    if int(forwardOrReverse)==0:
+        finger2forward(timeinput)
+    if int(forwardOrReverse)==1:
+        finger2reverse(timeinput)
     
-if int(z)==3:
-    if int(w)==0:
-        finger3(v)
-    if int(w)==1:
-        finger3_r(v)
+if int(fingerinput)==3:
+    if int(forwardOrReverse)==0:
+        finger3forward(timeinput)
+    if int(forwardOrReverse)==1:
+        finger3reverse(timeinput)
     
-if int(z)==4:
+if int(fingerinput)==4:
     
-    all(v)
+    allforward(timeinput)
     
-if int(z)==5:
+if int(fingerinput)==5:
     
-    reverse(v)
+    allreverse(timeinput)
